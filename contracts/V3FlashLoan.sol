21d6ee3170f30cd44963c59207ce2479b7966a07
lemkova <graceda64@gmail.com>
2022-04-20 14:53:00 +0700

    Update V3FlashLoan.sol
    add some comments

--- contracts/V3FlashLoan.sol
+++ contracts/V3FlashLoan.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract V3FlashLoan is FlashLoanSimpleReceiverBase, Ownable {
-    constructor(IPoolAddressesProvider _address) FlashLoanSimpleReceiverBase(_address){}
+    constructor(IPoolAddressesProvider _address) FlashLoanSimpleReceiverBase(_address){} // Use Pool Address Provider of Aave3 from your desired chain. 
 
     function executeOperation(
         address asset, 
@@ -21,6 +21,9 @@ contract V3FlashLoan is FlashLoanSimpleReceiverBase, Ownable {
         override
         returns (bool)
     {
+        // Do your things here
+        
+        // Make sure you have enough amounts of borrowed token + premium (interest) to pay your loan back.
         uint256 debt = amount + premium;
         IERC20(asset).approve(address(POOL), debt);
         return true;
@@ -28,7 +31,7 @@ contract V3FlashLoan is FlashLoanSimpleReceiverBase, Ownable {
 
     function startFlashLoan() public onlyOwner {
         address receiver = address(this);
-        address asset = address(0x5343b5bA672Ae99d627A1C87866b8E53F47Db2E6); // Fantom Testnet Aave mintable DAI
+        address asset = address(0x5343b5bA672Ae99d627A1C87866b8E53F47Db2E6); // Fantom Testnet Aave mintable DAI (Borrowed token)
 
         uint256 amount = 100000 * 10**18;
 
